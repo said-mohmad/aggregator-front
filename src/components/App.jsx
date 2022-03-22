ce7649fc48242ff00e4c1fe7070ac3f8d8e838b4
Alvi-soltakhanov <Alvi-soltakanov@mail.ru>
2022-03-22 14:27:30 +0300

    Страница поиска

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,11 @@
 import style from "./App.module.css";
+import SearchPage from "./SearchPage/SearchPage";
+
 
 function App() {
-  return <div className={style.app}></div>;
+  return <div className={style.app}>
+    <SearchPage />
+  </div>;
 }
 
 export default App;
