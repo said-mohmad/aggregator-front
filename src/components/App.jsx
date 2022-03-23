889508e140005bef10c1bfc3c6a639fb65925828
Muskhab Israilov <93041805+Muskhab@users.noreply.github.com>
2022-03-23 12:40:45 +0300

    Оптимизировал компоненты и код

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,15 @@
 import style from "./App.module.css";
 import Header from "./Header/Header";
-import Main from "./MainPage/Main";
-import Form from "./MainPage/SearchForm/Form";
 import SearchPage from "./SearchPage/SearchPage";
 import PersonPage from "./PersonPage/PersonPage";
 import Footer from "./Footer/Footer";
-import MainPage from '../components/MainPage/Main'
+import MainPage from "../components/MainPage/Main";
 
 const App = () => {
   return (
     <div className={style.app}>
       <Header />
       <MainPage />
-      <Form />
-      <Main />
       <SearchPage />
       <PersonPage />
       <Footer />
